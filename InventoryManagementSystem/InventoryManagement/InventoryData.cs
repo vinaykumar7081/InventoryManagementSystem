c48110275448413d672b19aa3cc006b085599dd5
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-25 15:12:21 +0530

    [VINAY] Add:Calculating the total price of the FoodGrains

---
+++ InventoryManagementSystem/InventoryManagement/InventoryData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace InventoryManagementSystem.InventoryManagement
+{
+    public class InventoryData
+    {
+        public void DisplayInventoryData(string filePath)
+        { 
+        StreamReader reader = new StreamReader(filePath);
+
+            string json = reader.ReadToEnd();
+            var inventory = JsonConvert.DeserializeObject<List<Inventory>>(json);
+            Console.WriteLine("Name"+"\t"+"PricePrKg"+"\t"+"Weight"+"\t\t"+"TotalPrice");
+            foreach (Inventory item in inventory)
+            {
+                Console.WriteLine(item.Name+ "\t" +item.Price+ "\t\t" +item.Weight+  "\t\t" +(item.Price * item.Weight));
+            }
+        }
+    }
+}
---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using InventoryManagementSystem.InventoryManagement;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome In The Inventory Management");
+        const string INVENTORY_FILE_PATH = @"D:\InventoryManagementProblemStatement\InventoryManagementSystem\InventoryManagementSystem\InventoryManagement\Inventory.json";
+        InventoryData inventoryData = new InventoryData();
+        Console.WriteLine("Display 1\n");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("Enter the Above Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                   inventoryData.DisplayInventoryData(INVENTORY_FILE_PATH);
+                    break;
+                case 2:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
